31598baf2bfd78972bec389a1a3836c31374715b
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2019-03-01 12:00:20 -0500

    done

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -2,4 +2,39 @@ class PizzasController < ApplicationController
   def index
     @pizzas = Pizza.all
   end
+
+  def show
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def new
+    @pizza = Pizza.new
+  end
+
+  def edit
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def create
+    pizza = Pizza.create(pizza_params)
+    if pizza.valid?
+      redirect_to pizza_path(pizza)
+    else
+      flash[:errors] = pizza.errors.full_messages
+      redirect_to new_pizza_path
+    end
+  end
+
+  def update
+    pizza = Pizza.find(params[:id])
+    pizza.update(pizza_params)
+    redirect_to pizza_path(pizza)
+  end
+
+  private
+
+  def pizza_params
+    params.require(:pizza).permit(:name, :ingredients, :restaurant_id)
+  end
+
 end
