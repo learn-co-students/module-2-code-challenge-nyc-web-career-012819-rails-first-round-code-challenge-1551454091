d6b6b8f2e388584b604aa5d70dc2c6bfd8e049d2
faizaniq <github email address>
2019-03-01 12:00:35 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get '/restaurants', to: 'restaurants#index'
-  get '/pizzas', to: 'pizzas#index'
+   get '/restaurants', to: 'restaurants#index'
+   get '/restaurants/:id', to: 'restaurants#show'
+   get '/pizzas', to: 'pizzas#index'
+   get '/pizzas/:id', to: 'pizzas#show'
+   get '/pizzas/new', to: 'pizzas#new'
+   post '/pizzas/new', to: 'pizzas#create'
+  # resource :restaurants
+  # resource :pizzas
 end
