7f827460a646d09f63f5ef39bb168e0bc0a79e58
github username <github email address>
2019-03-01 12:00:40 -0500

    test complete

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -2,4 +2,41 @@ class PizzasController < ApplicationController
   def index
     @pizzas = Pizza.all
   end
+
+  def show
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def new
+    @pizza = Pizza.new
+    @restaurant = Restaurant.all
+  end
+
+  def create
+    @pizza = Pizza.new(pizza_params)
+    @pizza.save
+    redirect_to pizza_path(@pizza)
+  end
+
+  def edit
+    @pizza = Pizza.find(params[:id])
+    @restaurant = Restaurant.all
+  end
+
+  def update
+    @pizza = Pizza.find(params[:id])
+    @pizza.update(pizza_params)
+    redirect_to pizza_path(@pizza)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def pizza_params
+    params.require(:pizza).permit(:name, :ingredients, :restaurant_id)
+
+  end
+
 end
