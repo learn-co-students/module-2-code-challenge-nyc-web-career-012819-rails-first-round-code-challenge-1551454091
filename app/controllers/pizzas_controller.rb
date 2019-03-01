e837e636f4f3d287e381a216db7d26f4779a729c
github username <github email address>
2019-03-01 12:02:20 -0500

    just validations missing

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,5 +1,50 @@
 class PizzasController < ApplicationController
   def index
     @pizzas = Pizza.all
+    render :index
+  end
+
+  def show
+    find_pizza
+    @restaurant = Restaurant.find(params[:id])
+    render :show
+  end
+
+  def new
+    @pizza = Pizza.new
+    render :new
+  end
+
+  def create
+    @restaurants = Restaurant.all
+    @pizza = Pizza.new(pizza_params)
+    if @pizza.save
+      redirect_to pizza_path(@pizza)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    find_pizza
+  end
+
+  def update
+    find_pizza
+    if @pizza.update(pizza_params)
+      redirect_to pizza_path(@pizza)
+    else
+      render :edit
+    end
+  end
+
+  def find_pizza
+    @pizza = Pizza.find(params[:id])
+  end
+
+  private
+
+  def pizza_params
+    params.require(:pizza).permit(:name, :ingredients, :restaurant_id)
   end
 end
