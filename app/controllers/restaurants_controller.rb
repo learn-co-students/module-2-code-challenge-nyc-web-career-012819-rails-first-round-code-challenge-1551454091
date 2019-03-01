56b1489060892a3834c5501d3b1b68cde2d79fff
github username <github email address>
2019-03-01 12:00:54 -0500

    almost done!

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,23 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @pizzas = Pizza.all
+  end
+
+
+
+
+
+
+
+  private
+
+  def rest_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+
+
 end
