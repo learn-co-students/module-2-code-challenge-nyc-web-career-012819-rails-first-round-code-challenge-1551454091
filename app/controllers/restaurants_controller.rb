dd2f4814e476051eb9941dd2bebd7ac1bdc1263d
Ethan Nam <ethan.s.nam@gmail.com>
2019-03-01 11:18:52 -0500

    Done.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,19 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @pizzas = @restaurant.pizzas
+  end
+
+  def search
+    restaurant = Restaurant.where(name: params[:q])
+    if !restaurant.empty?
+      redirect_to restaurant_path(restaurant.first)
+    else
+      flash[:error] = "No such restaurant exists!"
+      redirect_to restaurants_path
+    end
+  end
 end
