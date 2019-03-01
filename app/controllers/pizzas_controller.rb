634c9eaaebb6dc883c2858150e3c3a85534fad32
bdix1111 <brianna_dixon@rocketmail.com>
2019-03-01 12:01:23 -0500

    done

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,5 +1,38 @@
 class PizzasController < ApplicationController
   def index
     @pizzas = Pizza.all
+
+    render :index
+  end
+
+  def show
+    @pizza = Pizza.find(params[:id])
+    @ingredients = @pizza.ingredients
+
+    render :show
+  end
+
+  def new
+    @pizza = Pizza.new
+    @restaurants = Restaurant.all
+
+    render :new
+  end
+
+  def create
+    pizza = Pizza.create(pizza_params)
+
+    if pizza.valid?
+      redirect_to pizzas_path
+    else
+      flash[:errors] = pizza.errors.full_messages
+      redirect_to new_pizza_path
+    end 
+  end
+
+  private
+
+  def pizza_params
+    params.require(:pizza).permit(:name, :ingredients, :restaurant_id)
   end
 end
--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,7 @@
 class Pizza < ApplicationRecord
+
+belongs_to :restaurant
+
+validates :name, uniqueness: true
+
 end
