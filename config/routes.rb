e08bbbda0819c485572fdf22767c9bbc43614b1a
stomg7969 <stomg7969@gmail.com>
2019-03-01 11:42:05 -0500

    tried to add feature that accepts multiple ingredients and collect them in one array, but nope.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # resources :pizzas, only: [...]
+
   get '/restaurants', to: 'restaurants#index'
+  get '/restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+
   get '/pizzas', to: 'pizzas#index'
+  get '/pizzas/new', to: 'pizzas#new'
+  get '/pizzas/:id', to: 'pizzas#show', as: 'pizza'
+  post '/pizzas', to: 'pizzas#create'
 end
